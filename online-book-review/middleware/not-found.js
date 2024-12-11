1ec879d2c96419d77146e452d9f9f37973ee4ce8
Tanushree Mondal <66405927+tanu666@users.noreply.github.com>
2024-12-11 10:18:03 +0530

    Delete online-book-review directory

--- online-book-review/middleware/not-found.js
+++
@@ -1,5 +0,0 @@
-function notFoundHandler(req, res) {
-    return res.status(404).send(`<h1>Page Not Found!</h1>`);
-}
-
-export default notFoundHandler;
--- online-book-review/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as authControllers from "../controllers/auth.js";
-
-const router = Router();
-
-router.post("/auth/register", authControllers.register);
-router.post("/auth/login", authControllers.login);
-
-export default router;
